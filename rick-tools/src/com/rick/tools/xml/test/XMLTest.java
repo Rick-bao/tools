e4991b8cfea16d05a2b80f88915958f0033ac8ae
Rick <Rick@rick-computer>
2016-03-09 22:32:13 +0800

    rick 2016-03-09 22:32:01

--- rick-tools/src/com/rick/tools/xml/test/XMLTest.java
+++ rick-tools/src/com/rick/tools/xml/test/XMLTest.java
@@ -1,10 +1,19 @@
 package com.rick.tools.xml.test;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 import com.rick.tools.xml.XmlDocument;
 import com.rick.tools.xml.common.ConDefine;
 import com.rick.tools.xml.dom4j.Dom4jForXML;
+import com.rick.tools.xml.sax.SaxForXML;
 
 public class XMLTest {
 	
@@ -23,9 +32,20 @@ public class XMLTest {
 
 	/**
 	 * SAX测试方法入口
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException 
 	 */
 	@Test
-	public void saxTest(){
-		
+	public void saxTest() throws SAXException, IOException, ParserConfigurationException{
+		// step 1: 获得SAX解析器工厂实例
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+
+        // step 2: 获得SAX解析器实例
+        SAXParser parser = factory.newSAXParser();
+
+        // step 3: 开始进行解析
+        // 传入待解析的文档的处理器
+        parser.parse(new File(ConDefine.BK_PATH), new SaxForXML());
 	}
 }
